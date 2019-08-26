e351dec1e140cb2bf341bcb88667e8e2d8d91fa2
ytaconelli <ytaconelli31@gmail.com>
2019-08-26 01:52:19 -0300

    Programação finalizada, só restando ajeitar layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNota1;
     private EditText editTextNota2;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewMedia;
     private TextView textViewMaior;
     private TextView textViewMenor;
+    private TextView textViewResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +29,49 @@ public class MainActivity extends AppCompatActivity {
         textViewMedia = findViewById(R.id.text_view_media);
         textViewMaior = findViewById(R.id.text_view_maior);
         textViewMenor = findViewById(R.id.text_view_menor);
+        textViewResultado = findViewById((R.id.text_view_resultado));
     }
 
-    public void calculo(View v){
-    Double nota1 = Double.parseDouble(editTextNota1.getText().toString().replace(".",","));
-    Double nota2 = Double.parseDouble(editTextNota2.getText().toString().replace(".",","));
-    Double nota3 = Double.parseDouble(editTextNota3.getText().toString().replace(".",","));
-    Double soma = (nota1 + nota2 + nota3);
-    Double media = (soma /3);
+    public void calculo(View v) {
+        Double nota1 = Double.parseDouble(editTextNota1.getText().toString().replace(".", ","));
+        Double nota2 = Double.parseDouble(editTextNota2.getText().toString().replace(".", ","));
+        Double nota3 = Double.parseDouble(editTextNota3.getText().toString().replace(".", ","));
+        Double soma = (nota1 + nota2 + nota3);
+        Double media = (soma / 3);
+        textViewMedia.setText(media.toString());
 
-    textViewMedia.setText(media.toString());
+
+
+        if (nota1 > nota2 && nota1 > nota3) {
+            textViewMaior.setText(nota1.toString());
+        }
+        else if (nota2 > nota3){
+            textViewMaior.setText(nota2.toString());
+        }
+        else {
+            textViewMaior.setText(nota3.toString());
+        }
+
+
+        if (nota1 < nota2 && nota1 < nota3) {
+            textViewMenor.setText(nota1.toString());
+        }
+        else if (nota2 < nota3){
+            textViewMenor.setText(nota2.toString());
+        }
+        else {
+            textViewMenor.setText(nota3.toString());
+        }
+
+
+
+        if (media >= 7) {
+            textViewResultado.setText("Aprovado");
+        } else if (media < 7 && media >= 5) {
+            textViewResultado.setText("Recuperação");
+        } else if (media < 5) {
+            textViewResultado.setText("Reprovado");
+        }
+
     }
 }
